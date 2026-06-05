8b6b6ae03d7800ef0cfa3e94b135e7665c556d92
m-alex-n <alexnzioka42@gmail.com>
2026-06-05 23:45:26 +0300

    adding new file to enable pushing to vercel

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://school-system-backend-v8yw.onrender.com/api',
+        changeOrigin: true,
+      }
+    }
+  },
+  build : {
+    outDir: 'dist',
+  }
 })
